d733f4588754b3b093a61e704b0cbfacecb394fa
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-05-08 21:06:16 +0300

    done

--- src/Employee Salary History Tracker.cpp
+++ src/Employee Salary History Tracker.cpp
@@ -29,26 +29,25 @@ public:
     {
         return (head == NULL);
     }
-    void insertRecord(int id, string name, double previousSalary, double bonus, string dateOfChange)
+    void addEmployee(int id, string name, string dateOfChange, double previousSalary)
     {
-        employeeTracker *newNode = new employeeTracker();
-        newNode->previousSalary = previousSalary;
-        newNode->dateOfChange = dateOfChange;
-        newNode->bonus = bonus;
-        newNode->id = id;
-        newNode->name = name;
-        if (isEmpty())
+        if (doSearch(id))
         {
-            head = newNode;
-            newNode->arrow = NULL;
+            cout << "This Employee Exist";
         }
         else
         {
+            employeeTracker *newNode = new employeeTracker();
+            newNode->previousSalary = previousSalary;
+            newNode->dateOfChange = dateOfChange;
+            newNode->bonus = 0.0;
+            newNode->id = id;
+            newNode->name = name;
             newNode->arrow = head;
             head = newNode;
         }
     }
-    bool searchRecord(int id)
+    bool doSearch(int id)
     {
         employeeTracker *temp = head;
         while (temp != NULL)
@@ -61,9 +60,80 @@ public:
         }
         return false;
     }
+    string getName(int id)
+    {
+        employeeTracker *temp = head;
+        while (temp != NULL)
+        {
+            if (temp->id == id)
+            {
+                return temp->name;
+            }
+            temp = temp->arrow;
+        }
+        return "";
+    }
+    void insertRecord(int id, double previousSalary, double bonus, string dateOfChange)
+    {
+        if (!doSearch(id))
+        {
+            cout << "Doesn't Exist" << endl;
+        }
+        else
+        {
+            employeeTracker *newNode = new employeeTracker();
+            newNode->previousSalary = previousSalary;
+            newNode->dateOfChange = dateOfChange;
+            newNode->bonus = bonus;
+            newNode->id = id;
+            string name = getName(id);
+            newNode->name = name;
+            newNode->arrow = head;
+            head = newNode;
+        }
+    }
+    void displayAllEmployee()
+    {
+        if (isEmpty())
+        {
+            cout << "No Employee Founded" << endl;
+        }
+        else
+        {
+            employeeTracker *temp = head;
+            int empId[100];
+            string empName[100];
+            int curArr = 0;
+            while (temp != NULL)
+            {
+                bool found = false;
+                for (int i = 0; i < curArr; i++)
+                {
+                    if (temp->id == empId[i])
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    empId[curArr] = temp->id;
+                    empName[curArr] = temp->name;
+                    curArr++;
+                }
+                temp = temp->arrow;
+            }
+            for (int i = 0; i < curArr; i++)
+            {
+                cout << "Employee ID: " << empId[i] << endl
+                     << "Employee Name: " << empName[i] << endl;
+                cout << "---------------------------" << endl;
+            }
+        }
+    }
     void displayRecord(int id)
     {
-        if (searchRecord(id))
+        if (doSearch(id))
         {
             employeeTracker *temp = head;
             while (temp != NULL)
@@ -91,13 +161,13 @@ public:
     }
     void displayAllRecords()
     {
-        employeeTracker *temp = head;
-        if (temp == NULL)
+        if (isEmpty())
         {
-            cout << "No records found" << endl;
+            cout << "No Records Founded" << endl;
         }
         else
         {
+            employeeTracker *temp = head;
             while (temp != NULL)
             {
                 cout << "Employee ID: " << temp->id << endl
@@ -121,9 +191,11 @@ int main()
     do
     {
         cout << "===Employee Salary History Tracker===" << endl;
-        cout << "1 => Add New Record" << endl
-             << "2 => Display Record With ID" << endl
-             << "3 => Display All Record" << endl
+        cout << "1 => Add New Employee" << endl
+             << "2 => Add New Record" << endl
+             << "3 => Display Record With ID" << endl
+             << "4 => Display All Record" << endl
+             << "5 => Display All Employee" << endl
              << "0 => Exit Program" << endl
              << "------------------------------------" << endl;
         cout << "Enter Process: ";
@@ -136,24 +208,38 @@ int main()
             cin >> id;
             cout << "Enter Name: ";
             cin >> name;
+            cout << "Enter Initial Salary: ";
+            cin >> previousSalary;
+            cout << "Entre Date: ";
+            cin >> dateOfChange;
+            trackEmployee.addEmployee(id, name, dateOfChange, previousSalary);
+            cout << "------------------------------------" << endl;
+            break;
+        case 2:
+            cout << "Enter ID: ";
+            cin >> id;
             cout << "Enter Previous Salary: ";
             cin >> previousSalary;
             cout << "Entre Bonus: ";
             cin >> bonus;
             cout << "Entre Date: ";
             cin >> dateOfChange;
-            trackEmployee.insertRecord(id, name, previousSalary, bonus, dateOfChange);
+            trackEmployee.insertRecord(id, previousSalary, bonus, dateOfChange);
             cout << "------------------------------------" << endl;
             break;
-        case 2:
+        case 3:
             cout << "Enter ID: ";
             cin >> id;
             cout << "------------------------------------" << endl;
             trackEmployee.displayRecord(id);
             cout << "------------------------------------" << endl;
             break;
-        case 3:
+        case 4:
             trackEmployee.displayAllRecords();
+            cout << "------------------------------------" << endl;
+            break;
+        case 5:
+            trackEmployee.displayAllEmployee();
             cout << "------------------------------------" << endl;
             break;
         case 0:
